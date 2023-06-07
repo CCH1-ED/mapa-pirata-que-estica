8a572a877a4408e851ed4fb323b695cef52f377b
FBW1232 <fabianowelter3@gmail.com>
2023-06-07 11:07:59 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,8 +6,14 @@ int main(){
     int line, column;
     char command[10];
     scanf("%d %d \n", &line, &column);
+    for(int i = 0; i < line; i++){
+        for(int j = 0;j < column; i++){
+            
+        }
+    }
     while(command != 'F'){
         scanf("%s", &command);
+
     }
     return 0;
 }
