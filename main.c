bf64a6ac8dfdaa3768b7eb281b572fbd0dee3b5c
FBW1232 <fabianowelter3@gmail.com>
2023-06-07 11:02:50 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
-    
+    int line, column;
+    char command[10];
+    scanf("%d %d \n", &line, &column);
+    while(command != 'F'){
+        scanf("%s", &command);
+    }
+    return 0;
 }
