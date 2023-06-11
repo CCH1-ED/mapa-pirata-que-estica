c2ac396310431417046704f8cd1785e4bef856de
FBW1232 <fabianowelter3@gmail.com>
2023-06-11 01:20:15 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//pensei em criar uma pilha de vetores para armazenar a matriz e os dados dela mas sei la
+//pensei em criar uma pilha/fila de vetores para armazenar a matriz e os dados dela mas sei la
 
 typedef struct Nodo{
 
