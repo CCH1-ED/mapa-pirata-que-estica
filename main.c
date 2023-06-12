29a2fe22036d97affa525abd2fdabaf606df7e02
FBW1232 <fabianowelter3@gmail.com>
2023-06-12 12:10:36 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Nodo{
+    int vertice;
+    struct Nodo *next;
+}Nodo;
 
-char ** create_matrix(int line, int colunm){
-    char ** Line = (char **)malloc(sizeof(char *)*line);
+
+char **create_matrix(int line, int colunm){
+    char **Line = (char **)malloc(sizeof(char *)*line);
     for(int i; i < line;i++){
         Line[i] = (char *)malloc(sizeof(char)* colunm);
     }
@@ -16,15 +21,31 @@ char ** create_matrix(int line, int colunm){
     return Line;
 }
 
+Nodo **adjacency_list(int all_vertice,int line, int colunm,char **matrix){
+    Nodo ** adjacency = (Nodo **)malloc(sizeof(Nodo *)*all_vertice);
+    char vertice;
+    for(int i; i < line;i++){
+        for(int j; j < colunm;j++){
+            vertice = matrix[i][j];
+            if(vertice == '.'){
+                
+            }
+        }
+    }
+}
+
+Nodo **visitable(int line, int colunm,char **matrix){
+    
+}
+
 int main(){
     int line, colunm;
     scanf("%d %d", &line, &colunm);
-    char ** matrix = create_matrix(line, colunm);
-
+    char **matrix = create_matrix(line, colunm);
+    Nodo **adjacency_list(line, colunm, matrix);
     char command[10];
     do{
         scanf("%s", &command);
-        
     }while (command != 'F');
     
     return 0;
