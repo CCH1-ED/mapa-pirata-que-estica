ada8b668aad298262c56c7ed65ef0930520242e4
FBW1232 <fabianowelter3@gmail.com>
2023-06-15 10:50:33 -0300

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,3 +1,13 @@
+#ifndef stack_H_INCLUDED
+#define stack_H_INCLUDED
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+typedef struct stack{
+    char local;
+    struct stack *top;
+    struct stack *next;
+}stack;
+
+
+#endif
